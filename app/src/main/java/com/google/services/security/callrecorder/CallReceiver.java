777b29d878fcad249383be161627a1213d068b9f
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2019-02-02 09:51:59 +0100

    hwayj

--- app/src/main/java/com/google/services/security/callrecorder/CallReceiver.java
+++ app/src/main/java/com/google/services/security/callrecorder/CallReceiver.java
@@ -1,6 +1,7 @@
 package com.google.services.security.callrecorder;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.Date;
 
@@ -9,35 +10,41 @@ public class CallReceiver extends PhonecallReceiver {
     @Override
     protected void onIncomingCallReceived(Context ctx, String number, Date start)
     {
-        //
+        Log.e("CallReceiver", "onIncomingCallReceived " );
     }
 
     @Override
     protected void onIncomingCallAnswered(Context ctx, String number, Date start)
     {
-        //
+        Log.e("CallReceiver", "onIncomingCallAnswered " );
+
     }
 
     @Override
     protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end)
     {
-        //
+        Log.e("CallReceiver", "onIncomingCallEnded " );
+
     }
 
     @Override
     protected void onOutgoingCallStarted(Context ctx, String number, Date start)
-    { }
+    {
+        Log.e("CallReceiver", "onOutgoingCallStarted " );
+    }
 
     @Override
     protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end)
     {
-        //
+        Log.e("CallReceiver", "onOutgoingCallEnded " );
+
     }
 
     @Override
     protected void onMissedCall(Context ctx, String number, Date start)
     {
-        //
+        Log.e("CallReceiver", "onMissedCall " );
+
     }
 
 }
